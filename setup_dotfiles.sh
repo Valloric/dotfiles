d0c54fd5aaa1801c53744032666e577ddf2a2523
Strahinja Markovic <strahinja.markovic@gmail.com>
2011-10-16 22:10:28 -0700

    Moving code that was placed incorrectly
    The link creation code needs to go into the setup
    script, not into bashrc

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -13,8 +13,24 @@ fi
 for file in bash_aliases bash_completion
 do
   if [ ! -a ~/.$file ]
-  then 
+  then
     ln -s $script_dir/$file ~/.$file
   fi
 done
+
+
+# -p says to create the dir if it doesn't exist already
+mkdir -p $HOME/bin
+
+platform=$(uname)
+
+# Naturally, macvim needs to be installed for this to work
+if [[ $platform == *Darwin* ]]
+then
+    # symlinks for mvim
+    ln -s /usr/local/bin/mvim ~/bin/gvimdiff
+    ln -s /usr/local/bin/mvim ~/bin/vimdiff
+    ln -s /usr/local/bin/mvim ~/bin/vim
+    ln -s /usr/local/bin/mvim ~/bin/gvim
+fi
 
