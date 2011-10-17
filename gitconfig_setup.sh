64c676a31de01e13f218cd0b7d9e7f9c4e9e8708
Strahinja Markovic <strahinja.markovic@gmail.com>
2011-10-16 20:56:18 -0700

    On macs, we use opendiff (FileMerge) as diff tool

--- gitconfig_setup.sh
+++ gitconfig_setup.sh
@@ -21,7 +21,16 @@ git config --global alias.sub submodule
 
 git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
 
-git config --global diff.tool gvimdiff
+platform=$(uname)
+
+# If we are on mac, we use opendiff instead of gvimdiff
+if [[ $platform == *Darwin* ]]
+then
+    git config --global diff.tool opendiff
+else
+    git config --global diff.tool gvimdiff
+fi
+
 git config --global difftool.prompt false
 
 script_dir="$( cd "$( dirname "$0" )" && pwd )"
