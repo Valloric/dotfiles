0d5a67be1a55ccea08cea7a91640d7ef4661b32e
Strahinja Markovic <strahinja.markovic@gmail.com>
2011-10-18 22:36:08 -0700

    Our bashrc code is now added in more cases

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -4,15 +4,29 @@
 script_dir="$( cd "$( dirname "$0" )" && pwd )"
 
 # add bashrc_after to bashrc if it's not there already
-if ! grep -q bashrc_after.sh ~/.bashrc
+# the grep expression means "if the string IS NOT found in the file"
+if [[ ! -f ~/.bashrc || $( grep -q bashrc_after.sh ~/.bashrc ) ]]
 then
   echo "source $script_dir/bashrc_after.sh" >> ~/.bashrc
+fi
+
+platform=$(uname)
+
+# Mac OS X will not read .bashrc for terminal shells, but it will read
+# .bash_profile
+if [[ $platform == *Darwin* ]]
+then
+  # the grep expression means "if the string IS NOT found in the file"
+  if [[ ! -f ~/.bash_profile || $( grep -q bashrc_after.sh ~/.bash_profile ) ]]
+  then
+    echo "source $script_dir/bashrc_after.sh" >> ~/.bash_profile
+  fi
 fi
 
 # add links to our other files if there are not there already
 for file in aliases_bash.sh completion_bash.sh
 do
-  if [ ! -a ~/.$file ]
+  if [[ ! -a ~/.$file ]]
   then
     ln -s $script_dir/$file ~/.$file
   fi
@@ -22,17 +36,15 @@ done
 # -p says to create the dir if it doesn't exist already
 mkdir -p $HOME/bin
 
-platform=$(uname)
-
 # Naturally, macvim needs to be installed for this to work
 if [[ $platform == *Darwin* ]]
 then
-    # symlinks for mvim
-    ln -s /usr/local/bin/mvim ~/bin/gvimdiff
-    ln -s /usr/local/bin/mvim ~/bin/vimdiff
-    ln -s /usr/local/bin/mvim ~/bin/vim
-    ln -s /usr/local/bin/mvim ~/bin/gvim
-    ln -s /usr/local/bin/mvim ~/bin/vi
+  # symlinks for mvim
+  ln -s /usr/local/bin/mvim ~/bin/gvimdiff
+  ln -s /usr/local/bin/mvim ~/bin/vimdiff
+  ln -s /usr/local/bin/mvim ~/bin/vim
+  ln -s /usr/local/bin/mvim ~/bin/gvim
+  ln -s /usr/local/bin/mvim ~/bin/vi
 fi
 
 source gitconfig_setup.sh
