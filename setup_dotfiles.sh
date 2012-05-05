b069f5f24cf8d8ad9da59f75bf1c0d7b7863fc1a
Strahinja Val Markovic <val@markovic.io>
2012-05-05 16:15:01 -0700

    Updating the mvim symlinks for mac

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -38,11 +38,20 @@ platform=$(uname)
 if [[ $platform == *Darwin* ]]
 then
   # symlinks for mvim
-  ln -s /usr/local/bin/mvim ~/bin/gvimdiff
-  ln -s /usr/local/bin/mvim ~/bin/vimdiff
-  ln -s /usr/local/bin/mvim ~/bin/vim
-  ln -s /usr/local/bin/mvim ~/bin/gvim
-  ln -s /usr/local/bin/mvim ~/bin/vi
+  if [[ -a ~/bin/mvim ]]
+  then
+    ln -s ~/bin/mvim ~/bin/gvimdiff
+    ln -s ~/bin/mvim ~/bin/vimdiff
+    ln -s ~/bin/mvim ~/bin/vim
+    ln -s ~/bin/mvim ~/bin/gvim
+    ln -s ~/bin/mvim ~/bin/vi
+  else
+    ln -s /usr/local/bin/mvim ~/bin/gvimdiff
+    ln -s /usr/local/bin/mvim ~/bin/vimdiff
+    ln -s /usr/local/bin/mvim ~/bin/vim
+    ln -s /usr/local/bin/mvim ~/bin/gvim
+    ln -s /usr/local/bin/mvim ~/bin/vi
+  fi
 fi
 
 source $script_dir/gitconfig_setup.sh
