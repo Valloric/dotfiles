0ee983fadb5a1b224bb530cca592697567a6de99
Strahinja Val Markovic <strahinja@google.com>
2012-07-11 16:10:43 -0700

    Fix for wrong locations
    The computed location of the script is not correct when the script is executed
    from a different script. So we just hard-code the paths, it's not as if I'm
    going to change them.

--- tmux/tmux_setup.sh
+++ tmux/tmux_setup.sh
@@ -1,15 +1,12 @@
-# get the dir of the current script
-script_dir="$( cd "$( dirname "$0" )" && pwd )"
-
-cp -f $script_dir/tmux-main.conf ~/.tmux.conf
+cp -f $HOME/dotfiles/tmux/tmux-main.conf ~/.tmux.conf
 
 platform=$(uname)
 
 # we need to configure tmux differently depending on OS
 if [[ $platform == *Darwin* ]]
 then
-  cat $script_dir/tmux-osx.conf >> ~/.tmux.conf
+  cat $HOME/dotfiles/tmux/tmux-osx.conf >> ~/.tmux.conf
 else
-  cat $script_dir/tmux-linux.conf >> ~/.tmux.conf
+  cat $HOME/dotfiles/tmux/tmux-linux.conf >> ~/.tmux.conf
 fi
 
