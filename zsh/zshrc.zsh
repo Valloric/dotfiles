2df012996702cdc80b151c784812fdb3d1ca164b
Strahinja Val Markovic <val@markovic.io>
2013-05-20 19:25:40 -0700

    Turning off oh-my-zsh weekly update check
    OH GOD those were annoying...

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -10,7 +10,7 @@ ZSH=$HOME/.oh-my-zsh
 ZSH_THEME="valloric"
 
 # Comment this out to disable weekly auto-update checks
-# DISABLE_AUTO_UPDATE="true"
+DISABLE_AUTO_UPDATE="true"
 
 # Uncomment following line if you want to disable colors in ls
 # DISABLE_LS_COLORS="true"
