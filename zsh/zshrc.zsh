5a033272fd4bb0f6f917a06255d099c5a3927d11
Strahinja Val Markovic <val@markovic.io>
2015-09-05 21:13:22 -0700

    Disabling oh-my-zsh terminal auto-titling

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -16,7 +16,7 @@ DISABLE_AUTO_UPDATE="true"
 # DISABLE_LS_COLORS="true"
 
 # Uncomment following line if you want to disable autosetting terminal title.
-# DISABLE_AUTO_TITLE="true"
+DISABLE_AUTO_TITLE="true"
 
 # Uncomment following line if you want red dots to be displayed while waiting for completion
 COMPLETION_WAITING_DOTS="true"
