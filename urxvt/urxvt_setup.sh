b8404efde3af3ed4837187933ff36478d32a17c3
Strahinja Val Markovic <val@markovic.io>
2015-09-26 12:46:04 -0700

    Fix syntax errors in Xdefaults file
    Also added checks to catch this early next time.

--- urxvt/urxvt_setup.sh
+++ urxvt/urxvt_setup.sh
@@ -4,6 +4,12 @@ set -e
 
 script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+# Check the Xdefaults file for syntax errors, like ' chars in comments
+if command -v xrdb >/dev/null 2>&1;
+then
+  xrdb -n $script_dir/Xdefaults 2>&1 >/dev/null
+fi
+
 cp -f $script_dir/Xdefaults ~/.Xdefaults
 
 perl_ext=$script_dir/perl-extensions
