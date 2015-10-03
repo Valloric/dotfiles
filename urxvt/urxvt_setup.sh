923951ac7734397375cb07a62ffba2af5d53ded1
Strahinja Val Markovic <val@markovic.io>
2015-10-03 12:10:38 -0700

    More urxvt configuration
    If I see screwed-up letter-spacing in urxvt just ONE. MORE. TIME I'm
    going to lose my fucking mind.

--- urxvt/urxvt_setup.sh
+++ urxvt/urxvt_setup.sh
@@ -10,7 +10,10 @@ then
   xrdb -n $script_dir/Xdefaults 2>&1 >/dev/null
 fi
 
+# We want to set up both Xdefaults and Xresources; here's why:
+#   http://superuser.com/q/243914/436719
 cp -f $script_dir/Xdefaults ~/.Xdefaults
+cp -f $script_dir/Xdefaults ~/.Xresources
 
 perl_ext=$script_dir/perl-extensions
 perl -i -p -e "s!PATH_TO_PERL_EXT!$perl_ext!ig" ~/.Xdefaults
