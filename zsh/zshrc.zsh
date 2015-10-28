75cc3b249ad1a67cb8c820bb29f3c07a3fb09b0b
Strahinja Val Markovic <vmarkovic@linkedin.com>
2015-10-28 12:31:42 -0700

    fzf now using exact matching by default

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -76,7 +76,7 @@ export SVN_EDITOR="gvim"
 
 # FZF setup; https://github.com/junegunn/fzf
 export FZF_DEFAULT_OPTS="
---extended
+--extended-exact
 --color fg:252,bg:233,hl:67,fg+:252,bg+:235,hl+:81
 --color info:144,prompt:161,spinner:135,pointer:135,marker:118
 "
