7101cfe2fff2d50d24984c99e5bf7eff010bec81
Strahinja Val Markovic <val@markovic.io>
2015-11-11 14:05:51 -0800

    Intellij setup script handles mac now

--- intellij/intellij_setup.sh
+++ intellij/intellij_setup.sh
@@ -3,22 +3,36 @@
 set -e
 
 script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+platform=$(uname)
 
 if [[ ! -a ~/.ideavimrc ]]
 then
   ln -s $script_dir/ideavimrc ~/.ideavimrc
 fi
 
-mkdir -p ~/.IdeaIC14
-
-if [[ ! -a ~/.IdeaIC14/config ]]
+if [[ $platform == *Darwin* ]]
 then
-  ln -s $script_dir/config ~/.IdeaIC14/config
-fi
+  if [[ ! -a ~/Library/Preferences/IdeaIC14 ]]
+  then
+    ln -s $script_dir/config ~/Library/Preferences/IdeaIC14
+  fi
 
-mkdir -p ~/.IntelliJIdea14
+  if [[ ! -a ~/Library/Preferences/IntelliJIdea14 ]]
+  then
+    ln -s $script_dir/config ~/Library/Preferences/IntelliJIdea14
+  fi
+else
+  mkdir -p ~/.IdeaIC14
 
-if [[ ! -a ~/.IntelliJIdea14/config ]]
-then
-  ln -s $script_dir/config ~/.IntelliJIdea14/config
+  if [[ ! -a ~/.IdeaIC14/config ]]
+  then
+    ln -s $script_dir/config ~/.IdeaIC14/config
+  fi
+
+  mkdir -p ~/.IntelliJIdea14
+
+  if [[ ! -a ~/.IntelliJIdea14/config ]]
+  then
+    ln -s $script_dir/config ~/.IntelliJIdea14/config
+  fi
 fi
