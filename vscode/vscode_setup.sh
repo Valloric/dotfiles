fd5c71fc7ca6f6aa28ee2f3faa8f4998745a0e84
Val Markovic <val@markovic.io>
2019-11-09 18:18:30 -0800

    more vscode config

--- vscode/vscode_setup.sh
+++ vscode/vscode_setup.sh
@@ -6,20 +6,26 @@ declare script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 declare vsc_config_dir="$HOME/.config/Code/User"
 mkdir -p $vsc_config_dir
 
-if [[ ! -a $vsc_config_dir/settings.json ]]
-then
+if [[ ! -e $vsc_config_dir/settings.json ]]; then
   ln -s $script_dir/settings.json $vsc_config_dir/settings.json
 fi
 
-if [[ ! -a $vsc_config_dir/keybindings.json ]]
-then
+if [[ ! -e $vsc_config_dir/keybindings.json ]]; then
   ln -s $script_dir/keybindings.json $vsc_config_dir/keybindings.json
 fi
 
+# Get list of installed extensions with code --list-extensions
 declare -a extensions=(
   aaron-bond.better-comments
+  bbenoist.togglehs
+  DavidAnson.vscode-markdownlint
   foxundermoon.shell-format
+  llvm-vs-code-extensions.vscode-clangd
+  mrcrowl.hg
   ms-python.python
+  rust-lang.rust
+  streetsidesoftware.code-spell-checker
+  vscode-icons-team.vscode-icons
   vscodevim.vim
 )
 
