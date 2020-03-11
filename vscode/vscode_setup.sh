cfc2b66072f759a86e130dc173b7a92e27ca6a7f
Val Markovic <val@markovic.io>
2020-03-11 11:58:58 -0700

    Updating vscode setup for mac

--- vscode/vscode_setup.sh
+++ vscode/vscode_setup.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
 declare script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-declare vsc_config_dir="$HOME/.config/Code/User"
+if [[ $(uname) == *Darwin* ]]; then
+  declare vsc_root_dir="$HOME/Library/Application Support/Code"
+else
+  declare vsc_root_dir="$HOME/.config/Code"
+fi
+declare vsc_config_dir="$vsc_root_dir/User"
+
 mkdir -p $vsc_config_dir
 
 if [[ ! -e $vsc_config_dir/settings.json ]]; then
-  ln -s $script_dir/settings.json $vsc_config_dir/settings.json
+  ln -s $script_dir/settings.json "$vsc_config_dir/settings.json"
 fi
 
 if [[ ! -e $vsc_config_dir/keybindings.json ]]; then
-  ln -s $script_dir/keybindings.json $vsc_config_dir/keybindings.json
+  ln -s $script_dir/keybindings.json "$vsc_config_dir/keybindings.json"
 fi
 
 if [[ ! -e $vsc_config_dir/snippets ]]; then
-  ln -s $script_dir/snippets $vsc_config_dir/snippets
+  ln -s $script_dir/snippets "$vsc_config_dir/snippets"
 fi
 
 # Get list of installed extensions with code --list-extensions
@@ -40,7 +46,10 @@ for i in ${extensions[@]}; do
   code --install-extension $i &
 done
 
-declare vsc_dicts_dir="$HOME/.config/Code/Dictionaries"
+declare vsc_dicts_dir="$vsc_root_dir/Dictionaries"
 mkdir -p $vsc_dicts_dir
-ln -s /usr/share/hunspell/* $vsc_config_dir
-ln -s $script_dir/spellright.dict $vsc_config_dir
+
+if [[ $(uname) != *Darwin* ]]; then
+  ln -s /usr/share/hunspell/* $vsc_dicts_dir
+fi
+ln -s $script_dir/spellright.dict $vsc_dicts_dir
