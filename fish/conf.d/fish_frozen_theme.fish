7c9dac57dea7998b435f66295ca59753f4927aa4
Val Markovic <val@markovic.io>
2026-06-04 11:56:57 +0200

    Add fish theme chosen with fish_config
    The filename doesn't follow our `##-name` convention because this is the
    name fish chose and expects.

---
+++ fish/conf.d/fish_frozen_theme.fish
@@ -0,0 +1,37 @@
+# Don't edit this file, as it will be written by the web-config tool (`fish_config`).
+# To customize your theme, delete this file and see
+#     help interactive#syntax-highlighting
+# or
+#     man fish-interactive | less +/^SYNTAX.HIGHLIGHTING
+# for appropriate commands to add to ~/.config/fish/config.fish instead.
+
+set --global fish_color_autosuggestion 707A8C
+set --global fish_color_cancel --reverse
+set --global fish_color_command 5CCFE6
+set --global fish_color_comment 5C6773
+set --global fish_color_cwd 73D0FF
+set --global fish_color_cwd_root red
+set --global fish_color_end F29E74
+set --global fish_color_error FF3333
+set --global fish_color_escape 95E6CB
+set --global fish_color_history_current --bold
+set --global fish_color_host --reset
+set --global fish_color_host_remote yellow
+set --global fish_color_normal CBCCC6
+set --global fish_color_operator FFCC66
+set --global fish_color_param CBCCC6
+set --global fish_color_quote BAE67E
+set --global fish_color_redirection D4BFFF
+set --global fish_color_search_match --bold --background=FFCC66
+set --global fish_color_selection --bold --background=FFCC66
+set --global fish_color_status red
+set --global fish_color_user brgreen
+set --global fish_color_valid_path --underline=single
+set --global fish_pager_color_completion --reset
+set --global fish_pager_color_description B3A06D
+set --global fish_pager_color_prefix --bold --underline=single
+set --global fish_pager_color_progress brwhite --bold --background=cyan
+set --global fish_pager_color_selected_background --background=FFCC66
+set --global fish_pager_color_selected_completion 
+set --global fish_pager_color_selected_description 
+set --global fish_pager_color_selected_prefix 
